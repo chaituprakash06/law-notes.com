259bbb34a037e20200c3ee005d0911f2f3e9ed42
Chaitanya Prakash <chaituprakash06@gmail.com>
2025-05-08 23:01:41 +0400

    2025-04-30.basil

--- lib/stripe-server.ts
+++ lib/stripe-server.ts
@@ -2,7 +2,7 @@ import Stripe from 'stripe';
 
 // Initialize server-side Stripe
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2023-10-16', // Use the latest Stripe API version
+  apiVersion: '2025-04-30.basil', // Use the latest Stripe API version
 });
 
 export { stripe };
